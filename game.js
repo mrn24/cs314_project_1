46925743ff90999d8b2d1b4c3bf1e161472e074a
mrn24 <mrn24@nau.edu>
2016-05-18 16:01:47 -0700

    Fixed boundaries, protected spawn/finish from whirls

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var gameport = document.getElementById("gameport");
 var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x00BFFF});
 gameport.appendChild(renderer.view);
 
-var background = PIXI.Sprite.fromImage("button_test_BG.jpg");
+var background = PIXI.Sprite.fromImage("BG.jpg");
 
 var stage = new PIXI.Container();
 
@@ -14,10 +14,13 @@ var texture = PIXI.Texture.fromImage("hoegarth.png");
 var whirltex = PIXI.Texture.fromImage("whirlpool.png");
 var finishtex = PIXI.Texture.fromImage("finishline.png");
 var endgameText = new PIXI.Text("You were sucked in a whirlpool!", {font:"25px Arial", fill:"yellow"});
+var endgameText2 = new PIXI.Text("Hit Space to try again!", {font:"25px Arial", fill:"yellow"});
+endgameText2.position.y = 20;
 
 var character = new PIXI.Sprite(texture);
 var finishLine = new PIXI.Sprite(finishtex);
 
+var isRunning = true;
 var level = 1;
 var levelBoard = new PIXI.Text("Level: " + level, {font:"10px Arial", fill:"yellow"});
 
@@ -47,38 +50,59 @@ finishLine.position.y = 380;
 stage.addChild(character);
 stage.addChild(finishLine);
 
-function fiveMore(){
-  for (var i = 0; i < 5; i++){
+function gameStart(){
+  isRunning = true;
+  character.rotation = 0;
+  character.position.x = 20;
+  character.position.y = 20;
+  for (var j = 0; j <= whirls.length; j++){
+    stage.removeChild(whirls[j]);
+  }
+  whirls = [];
+  for (var i = 0; i < level*5; i++){
     var whirlpool = new PIXI.Sprite(whirltex);
     whirlpool.anchor.x = 0.5;
     whirlpool.anchor.y = 0.5;
     whirlpool.position.x = Math.floor(Math.random() * 360) + 50;
     whirlpool.position.y = Math.floor(Math.random() * 360) + 50;
-    stage.addChild(whirlpool);
-    var newLength = whirls.push(whirlpool);
+    if ((whirlpool.position.x < 60 && whirlpool.position.y < 60)||(whirlpool.position.x > 340 && whirlpool.position.y > 340)){
+      i--;
+    }
+    else{
+      stage.addChild(whirlpool);
+      var newLength = whirls.push(whirlpool);
+    }
   }
 }
-fiveMore();
 
 function keydownEventHandler(e){
   if (e.keyCode == 87){//W
-    if(character.position.y > 0){
+    if(character.position.y > 20){
       character.position.y -= 10;
     }
   }
   if (e.keyCode == 83){//S
-    if(character.position.y < 400){
+    if(character.position.y < 380){
       character.position.y += 10;
     }
   }
   if (e.keyCode == 65){//A
-    if(character.position.x > 0){
+    if(character.position.x > 20){
       character.position.x -= 10;
     }
   }
   if (e.keyCode == 68){//D
-    if(character.position.x < 400){
+    if(character.position.x < 380){
       character.position.x += 10;
+    }
+  }
+  if (e.keyCode == 32){//Space
+    if (isRunning == false){
+      stage.removeChild(endgameText);
+      stage.removeChild(endgameText2);
+      level = 1;
+      levelBoard.setText("Level: " + level);
+      gameStart();
     }
   }
 }
@@ -86,13 +110,17 @@ function keydownEventHandler(e){
 document.addEventListener('keydown', keydownEventHandler);
 
 function endgame(){
+  isRunning = false;
   stage.addChild(endgameText);
+  stage.addChild(endgameText2);
+  level = 0;
+  levelBoard.setText("Level: " + level);
 }
 
 
 function animate(){
   requestAnimationFrame(animate);
-  for(var i = 0; i < level*5; i++){
+  for(var i = 0; i < whirls.length; i++){
     whirls[i].rotation += 0.1;
     if (Math.abs(character.position.x - whirls[i].position.x) <= 40 && Math.abs(character.position.y - whirls[i].position.y) <= 40){
       character.position.x = whirls[i].position.x;
@@ -103,11 +131,10 @@ function animate(){
     if (Math.abs(character.position.x - finishLine.position.x) <= 5 && Math.abs(character.position.y - finishLine.position.y) <= 5){
       level += 1;
       levelBoard.setText("Level: " + level);
-      fiveMore();
-      character.position.x = 20;
-      character.position.y = 20;
+      gameStart();
     }
   }
   renderer.render(stage);
 }
+gameStart();
 animate();
