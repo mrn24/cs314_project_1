c993e23dc777c2d0e58dc46215717b9f96ed545c
mrn24 <mrn24@nau.edu>
2016-05-18 10:38:57 -0700

    Made whirlpools, collision detection.

--- game.js
+++ game.js
@@ -11,9 +11,11 @@ var stage = new PIXI.Container();
 stage.addChild(background);
 
 var texture = PIXI.Texture.fromImage("hoegarth.png");
-var whirltex = PIXI.Texture.fromImage("Whirlpool.png");
+var whirltex = PIXI.Texture.fromImage("whirlpool.png");
+var finishtex = PIXI.Texture.fromImage("finishline.png");
 
 var character = new PIXI.Sprite(texture);
+var finishLine = new PIXI.Sprite(finishtex);
 
 var level = 1;
 var whirls = [];
@@ -24,19 +26,25 @@ character.scale.y = 0.2;
 character.anchor.x = 0.5;
 character.anchor.y = 0.5;
 
+finishLine.anchor.x = 0.5;
+finishLine.anchor.y = 0.5;
+
 character.position.x = 20;
 character.position.y = 20;
 
+finishLine.position.x = 380;
+finishLine.position.y = 380;
+
 stage.addChild(character);
+stage.addChild(finishLine);
 
 for (var i = 0; i < level*5; i++){
   var whirlpool = new PIXI.Sprite(whirltex);
-  whirlpool.scale.x = 0.2;
-  whirlpool.scale.y = 0.2;
   whirlpool.anchor.x = 0.5;
   whirlpool.anchor.y = 0.5;
   whirlpool.position.x = Math.floor(Math.random() * 380) + 10;
   whirlpool.position.y = Math.floor(Math.random() * 380) + 10;
+  stage.addChild(whirlpool);
   var newLength = whirls.push(whirlpool);
 }
 
@@ -61,7 +69,16 @@ function animate(){
   requestAnimationFrame(animate);
   for(var i = 0; i < level*5; i++){
     whirls[i].rotation += 0.1;
+    if (whirls[i].position.x - character.position.x < 10 || whirls[i].position.x - character.position.x > -10 || whirls[i].position.y - character.position.y < 10 || whirls[i].position.y - character.position.y > -10){
+      character.position.x = whirls[i].position.x;
+      character.position.y = whirls[i].position.y;
+      endgame();
+    }
   }
   renderer.render(stage);
 }
 animate();
+
+function endgame(){
+  pass;
+}
